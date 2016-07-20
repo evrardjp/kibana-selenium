d732cb837bf9730ff238331a8b440cf43cc8ffef
Ryan Dorothy <ryan.dorothy@mlh0bzfft3.rackspace.corp>
2016-07-20 16:12:57 -0500

    added a few directories for future use. Also created two tests but have not verified they work

---
+++ testrepo/kibana/kibana.py
@@ -0,0 +1,28 @@
+import unittest
+from selenium import webdriver
+
+
+class KibanaOne(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.PhantomJS()
+        self.driver.set_window_size(1120, 550)
+
+    def test_login(self):
+        self.driver.get("http://kibana:4345f83228a74062335ec75caa5bceacf597bd549662389b50b@204.232.187.36:8443/")
+        header = self.driver.find_element_by_css_selector('.brand').text
+        self.assertIn("Event Dashboard", header)
+
+    def test_graph_exists(self):
+        self.driver.get("http://kibana:4345f83228a74062335ec75caa5bceacf597bd549662389b50b@204.232.187.36:8443/")
+        a = self.driver.find_element_by_class_name('kibana-container')
+        b = a.find_element_by_class_name('kibana-row')
+        c = b.find_element_by_class_name('row-control')
+        d = c.find_element_by_class_name('panel-container')
+        self.assertIn("OPENSTACK EVENTS", d.text)
+
+    def tearDown(self):
+        self.driver.quit()
+
+if __name__ == '__main__':
+    unittest.main()
