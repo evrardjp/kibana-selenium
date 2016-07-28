3aefc75074ebf299916ae8ee245369c1e748d9a4
Ryan Dorothy <ryan.dorothy@mlh0bzfft3.rackspace.corp>
2016-07-28 13:14:32 -0500

    refactored all variables into the app.yaml file within config/. Created a class to load the configs. Added raise statements to all of the try catches in order to properly report failures.

--- testrepo/kibana/kibana.py
+++ testrepo/kibana/kibana.py
@@ -1,56 +1,79 @@
+import config
 import time
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support import expected_conditions as ec
 
 
 class KibanaOne(unittest.TestCase):
 
     def setUp(self):
-        """
-        :service_args: configuration management for phantomjs
-         This is used for testing headless and locally.
-        """
-        service_args = ['--proxy=localhost:9999', '--proxy-type=socks5']
-        #self.driver = webdriver.PhantomJS(service_args=service_args)
-        """
-        Ensure self.driver = webdriver.PhantomJS() is uncommented for remote testing.
-        """
-        self.driver = webdriver.PhantomJS()
-        self.driver.set_window_size(1120, 550)
-        """This will create the session within which all actions take place"""
-        self.driver.get("http://kibana:4345f83228a74062335ec75caa5bceacf597bd549662389b50b@204.232.187.36:8443/")
-        """element is used to ensure the page has fully loaded before we start accessing the page elements"""
-        element = WebDriverWait(self.driver, 10).until(
-            EC.text_to_be_present_in_element((By.CSS_SELECTOR,".brand"), "Event Dashboard")
-            )
-        time.sleep(1)
+        try:
+            """
+            :service_args: configuration management for phantomjs
+             This is used for testing headless and locally.
+            """
+            service_args = ['--proxy=localhost:9999', '--proxy-type=socks5']
+
+            """
+            Ensure self.driver = webdriver.PhantomJS()
+            is uncommented for remote testing.
+            """
+            self.driver = webdriver.PhantomJS(service_args=service_args)
+            # self.driver = webdriver.PhantomJS()
+            self.driver.set_window_size(1920, 1080)
+
+            """
+            This will create the session within which all actions take place
+            """
+            conf = config.app['kibana']
+            user = conf['username']
+            passwd = conf['password']
+            ext_vip = conf['external_vip']
+            url = "http://{0}:{1}@{2}:8443/".format(user, passwd, ext_vip)
+            self.driver.get(url)
+
+            """
+            element is used to ensure the page has fully
+            loaded before we start accessing the page elements
+            """
+            element = WebDriverWait(self.driver, 10).until(
+                ec.text_to_be_present_in_element((By.CSS_SELECTOR, ".brand"),
+                                                 "Event Dashboard")
+                )
+            time.sleep(1)
+        except Exception, e:
+            self.driver.save_screenshot('setup.png')
+            raise
 
     def test_login(self):
         try:
             header = self.driver.find_element_by_css_selector('.brand').text
             self.assertIn("Event Dashboard", header)
-        except Exception,e:
+        except Exception, e:
             self.driver.save_screenshot('login_ss.png')
+            raise
 
     def test_graph_exists(self):
         try:
-            a = self.driver.find_element_by_class_name('kibana-container')
-            b = a.find_element_by_class_name('kibana-row')
-            c = b.find_element_by_class_name('row-control')
-            d = c.find_element_by_class_name('panel-container')
-            self.assertIn("OPENSTACK EVENTS", d.text)
-        except Exception,e:
+            k_con = self.driver.find_element_by_class_name('kibana-container')
+            kib_row = k_con.find_element_by_class_name('kibana-row')
+            row = kib_row.find_element_by_class_name('row-control')
+            panel = row.find_element_by_class_name('panel-container')
+            self.assertIn("OPENSTACK EVENTS", panel.text)
+        except Exception, e:
             self.driver.save_screenshot('graph_exists_ss.png')
+            raise
 
     def test_log_section_exists(self):
         try:
             logs = self.driver.find_element_by_class_name("table-hover")
             self.assertIn("@timestamp host module logmessage", logs.text)
-        except Exception,e:
+        except Exception, e:
             self.driver.save_screenshot('log_section_ss.png')
+            raise
 
     def test_log_content_exists(self):
         try:
@@ -58,7 +81,8 @@ class KibanaOne(unittest.TestCase):
             pointer = self.driver.find_element_by_css_selector('tr.pointer')
             pointer.click()
             self.driver.implicitly_wait(1)
-            table = self.driver.find_element_by_css_selector('table.table-details')
+            table = self.driver.find_element_by_css_selector(
+                'table.table-details')
             tbody = table.find_element_by_css_selector('tbody')
             log = tbody.find_elements_by_tag_name('tr')
             for i, v in enumerate(log, start=1):
@@ -70,8 +94,9 @@ class KibanaOne(unittest.TestCase):
             css = "tr.ng-scope:nth-child({0}) > td:nth-child(3)".format(child)
             mess_content = tbody.find_element_by_css_selector(css).text
             self.assertIsNotNone(mess_content)
-        except Exception,e:
+        except Exception, e:
             self.driver.save_screenshot('log_content_ss.png')
+            raise
 
     def tearDown(self):
         self.driver.quit()
