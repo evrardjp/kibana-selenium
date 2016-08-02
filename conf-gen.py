bc190b87629950faa198d96a3e03ae8a404d80ca
Ryan Dorothy <ryan.dorothy@mlh0bzfft3.rackspace.corp>
2016-08-02 12:22:07 -0500

    created a method to return the external_lb_vip

--- conf-gen.py
+++ conf-gen.py
@@ -1,14 +1,15 @@
-from subprocess import call
+import subprocess
 
-host = call(["hostname"])
 
-if "qe-iad3-3" in host:
-    h = "qe-iad3-lab03"
-elif "qe-iad3-2" in host:
-    h = "qe-iad3-lab02"
-else:
-    raise Exception("conf-gen.py is only used to configure for iad3-2 and iad3-3 at this time.")
+def get_external_lb_vip():
+    host = subprocess.check_output(["hostname"])
+    if "qe-iad3-3" in host:
+        h = "qe-iad3-lab03"
+    elif "qe-iad3-2" in host:
+        h = "qe-iad3-lab02"
+    else:
+        raise Exception("conf-gen.py is used to configure for iad3-2 and iad3-3.")
 
-inv = "/opt/rpc-openstack/jenkins-oa/inventory/group_vars/{0}.yml".format(h)
-elva = call(["grep", "-R", "external_lb_vip_address", inv])
-print elva
+    inv = "/opt/rpc-openstack/jenkins-oa/inventory/group_vars/{0}.yml".format(h)
+    elva = subprocess.check_output(["grep", "-R", "external_lb_vip_address", inv])
+    return elva
