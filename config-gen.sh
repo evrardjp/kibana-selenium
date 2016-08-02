4e7418018b7f6540b3080da507c6a8d08c3e4f39
Ryan Dorothy <ryan.dorothy@mlh0bzfft3.rackspace.corp>
2016-08-02 15:13:51 -0500

    removed the bash config-gen.sh file

--- config-gen.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-password="$(grep -R "kibana_password" /etc/openstack_deploy/user_extras_secrets.yml)"
-external_lb_vip_address="$(grep -R "external_lb_vip_address" /opt/rpc-openstack/jenkins-oa/inventory/group_vars/qe-iad3-lab03.yml)"
-
-echo "kibana:" >> ryan.configuration
-echo "  ${password}" >> ryan.configuration
-echo "${external_lb_vip_address}" >> ryan.configuration
-echo "  username: kibana" >> ryan.configuration
