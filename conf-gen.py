38e3be96cf66d8263522f19e8d86fc3d1359f654
Ryan Dorothy <ryan.dorothy@mlh0bzfft3.rackspace.corp>
2016-08-03 13:41:23 -0500

    refactored for pep8 compliance

--- conf-gen.py
+++ conf-gen.py
@@ -8,10 +8,11 @@ def get_external_lb_vip():
     elif "qe-iad3-2" in host:
         h = "qe-iad3-lab02"
     else:
-        raise Exception("conf-gen.py is used to configure for iad3-2 and iad3-3.")
+        raise Exception("conf-gen.py is for iad3-2 and iad3-3.")
 
-    inv = "/opt/rpc-openstack/jenkins-oa/inventory/group_vars/{0}.yml".format(h)
-    elva = subprocess.check_output(["grep", "-R", "external_lb_vip_address", inv])
+    fi = "/opt/rpc-openstack/jenkins-oa/inventory/group_vars/{0}.yml".format(h)
+    elva = subprocess.check_output([
+        "grep", "-R", "external_lb_vip_address", fi])
     return elva
 
 
