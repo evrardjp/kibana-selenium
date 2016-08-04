f8054a9243e56454985c1f6f3e89e8cb89252715
Ryan Dorothy <ryan.dorothy@mlh0bzfft3.rackspace.corp>
2016-08-04 11:22:40 -0500

    refactored the config generator. Iad3-02 is deployed differently and the inventory file is in a different location than that of lab3-03. The new logic should support this fact.

--- conf-gen.py
+++ conf-gen.py
@@ -5,12 +5,12 @@ def get_external_lb_vip():
     host = subprocess.check_output(["hostname"])
     if "qe-iad3-3" in host:
         h = "qe-iad3-lab03"
+        fi = "/opt/rpc-openstack/jenkins-oa/inventory/group_vars/{0}.yml".format(h)
     elif "qe-iad3-2" in host:
-        h = "qe-iad3-lab02"
+        fi = "/etc/openstack_deploy/openstack_user_config.yml"
     else:
         raise Exception("conf-gen.py is for iad3-2 and iad3-3.")
 
-    fi = "/opt/rpc-openstack/jenkins-oa/inventory/group_vars/{0}.yml".format(h)
     elva = subprocess.check_output([
         "grep", "-R", "external_lb_vip_address", fi])
     return elva
